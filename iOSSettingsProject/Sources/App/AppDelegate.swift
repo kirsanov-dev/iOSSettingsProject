5906072833bfd766702ff4fca28c19f771afac9d
Oleg Kirsanov <olegkirsanov22@gmail.com>
2021-11-05 22:19:31 +0300

    Finish updating project to MVC pattern.

--- iOSSettingsProject/Sources/App/AppDelegate.swift
+++ iOSSettingsProject/Sources/App/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
-        let navigationController = UINavigationController(rootViewController: ViewController())
+        let navigationController = UINavigationController(rootViewController: SettingsViewController())
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
         self.window = window
